4a6818193347324282f5f327b5ba7400a1784ff6
lance <lancej2004@gmail.com>
2023-11-09 10:47:15 -0700

    j

--- UserTests/UserTest.cs
+++ UserTests/UserTest.cs
@@ -3,9 +3,11 @@ namespace UserTests
     public class UserTest
     {
         [Fact]
-        public void IsCreated()
+        public void Constructor_WorkingProperly()
         {
-
+            var User = new User("Lance");
+            Assert.Equal("Lance", User.Name);
+            Assert.Equal();
         }
     }
 }
